7c9f2e68f63034fc6502d45e44e72d1325c48c15
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-06-07 02:59:04 +0400

    add test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import App from "./App";
+import Gist from "./components/Gist";
+import GistDetails from "./components/GistDetails";
+import { Provider } from "react-redux";
+import Search from "./components/Search";
+import { gistDetails } from "./mocks";
+import store from "./redux/store";
+
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByTestId('app')
+  const linkElement = screen.getByTestId("app");
   expect(linkElement).toBeInTheDocument();
+});
+
+test("should show loader when API is called", () => {
+  render(
+    <Provider store={store}>
+      <Gist />
+    </Provider>
+  );
+  const loaderElement = screen.getByTestId("loader");
+  expect(loaderElement).toBeInTheDocument();
+});
+
+test("Should render gist list component", () => {
+  render(
+    <Provider store={store}>
+      <Gist />
+    </Provider>
+  );
+  const gistlistElement = screen.getByTestId("gist-list-items");
+  expect(gistlistElement).toBeInTheDocument();
+});
+
+test("Should render gist details component", () => {
+  render(
+    <Provider store={store}>
+      <GistDetails gist={gistDetails} />
+    </Provider>
+  );
+  const gistlistItemsElement = screen.getByTestId("gist-details");
+  expect(gistlistItemsElement).toBeInTheDocument();
+});
+
+test("check value of search input change", () => {
+  const handleInput = jest.fn();
+
+  const { container } = render(
+    <Provider store={store}>
+      <Search />
+    </Provider>
+  );
+  const inputNode = container.querySelector("input");
+  const searchString = "test user";
+  inputNode.focus();
+  inputNode.onChange = handleInput;
+  fireEvent.change(inputNode, { target: { value: searchString } });
+  expect(inputNode).toHaveValue(searchString);
 });
