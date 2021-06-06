375d2f161249bcc9014f2949e959a00ee7f0efac
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-06-07 02:58:35 +0400

    add mock data

---
+++ src/mocks/index.js
@@ -0,0 +1,52 @@
+export const gistDetails = {
+  url: "https://api.github.com/gists/a4254644b15c1b8682a9e45d81496fde",
+  forks_url:
+    "https://api.github.com/gists/a4254644b15c1b8682a9e45d81496fde/forks",
+  commits_url:
+    "https://api.github.com/gists/a4254644b15c1b8682a9e45d81496fde/commits",
+  id: "a4254644b15c1b8682a9e45d81496fde",
+  node_id: "MDQ6R2lzdGE0MjU0NjQ0YjE1YzFiODY4MmE5ZTQ1ZDgxNDk2ZmRl",
+  git_pull_url: "https://gist.github.com/a4254644b15c1b8682a9e45d81496fde.git",
+  git_push_url: "https://gist.github.com/a4254644b15c1b8682a9e45d81496fde.git",
+  html_url: "https://gist.github.com/a4254644b15c1b8682a9e45d81496fde",
+  files: {
+    "pilot.txt": {
+      filename: "pilot.txt",
+      type: "text/plain",
+      language: "Text",
+      raw_url:
+        "https://gist.githubusercontent.com/compcon/a4254644b15c1b8682a9e45d81496fde/raw/90c62aae4b876445406efd655c457882d2a794b4/pilot.txt",
+      size: 4736,
+    },
+  },
+  public: true,
+  created_at: "2021-06-06T21:20:34Z",
+  updated_at: "2021-06-06T21:20:34Z",
+  description: "Mouse - Collete Arkinis (COPY) (LL:4)",
+  comments: 0,
+  user: null,
+  comments_url:
+    "https://api.github.com/gists/a4254644b15c1b8682a9e45d81496fde/comments",
+  owner: {
+    login: "compcon",
+    id: 50460217,
+    node_id: "MDQ6VXNlcjUwNDYwMjE3",
+    avatar_url: "https://avatars.githubusercontent.com/u/50460217?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/compcon",
+    html_url: "https://github.com/compcon",
+    followers_url: "https://api.github.com/users/compcon/followers",
+    following_url:
+      "https://api.github.com/users/compcon/following{/other_user}",
+    gists_url: "https://api.github.com/users/compcon/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/compcon/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/compcon/subscriptions",
+    organizations_url: "https://api.github.com/users/compcon/orgs",
+    repos_url: "https://api.github.com/users/compcon/repos",
+    events_url: "https://api.github.com/users/compcon/events{/privacy}",
+    received_events_url: "https://api.github.com/users/compcon/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  truncated: false,
+};
